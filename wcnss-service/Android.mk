e115b778219a18f3f976025f5789b75b307c8b41
Hardik Kantilal Patel <hkpatel@codeaurora.org>
2013-12-03 12:37:42 +0530

    wcnss_service: Added support to read wlan mac from Modem NV
    Added support to read wlan mac from Modem NV using QMI interface.
    
    Change-Id: Iff732338cad57767e856209d70cf3af6fb0a8a56
    CRs-Fixed: 583465

--- wcnss-service/Android.mk
+++ wcnss-service/Android.mk
@@ -5,7 +5,19 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := wcnss_service
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/common/inc/
 LOCAL_SRC_FILES := wcnss_service.c
-LOCAL_SHARED_LIBRARIES := libc libcutils libutils
+ifeq ($(strip $(TARGET_USES_QCOM_WCNSS_QMI)),true)
+LOCAL_CFLAGS += -DWCNSS_QMI
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi/inc
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi/services
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi/platform
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi/src
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi/core/lib/inc
+LOCAL_SRC_FILES += wcnss_qmi_client.c
+endif #TARGET_USES_QCOM_WCNSS_QMI
+LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
+ifeq ($(strip $(TARGET_USES_QCOM_WCNSS_QMI)),true)
+LOCAL_SHARED_LIBRARIES += libqmiservices libqmi libqcci_legacy libqmi_client_qmux
+endif #TARGET_USES_QCOM_WCNSS_QMI
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wall
 include $(BUILD_EXECUTABLE)
