1781af5a8d7456508f0000b9063833d11350262c
Ashwini Patil <apati@codeaurora.org>
2016-03-28 11:46:00 +0530

    Wifi-HAl: Reduce multiple inclusion of qca-vendor.h
    Include qca-vendor.h file in vendor_definitions.h. And So,
    remove the qca-vendor.h file inclusion from all other files.
    
    Change-Id: I9e8105e73cf3ef96b23fefcd694edb0ebf1a3c24
    CRs-Fixed: 996140

--- qcwcn/wifi_hal/cpp_bindings.cpp
+++ qcwcn/wifi_hal/cpp_bindings.cpp
@@ -38,7 +38,7 @@
 #include "wifi_hal.h"
 #include "common.h"
 #include "cpp_bindings.h"
-#include "qca-vendor.h"
+#include "vendor_definitions.h"
 
 void appendFmt(char *buf, size_t buf_len, int &offset, const char *fmt, ...)
 {
