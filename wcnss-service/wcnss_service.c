14ceb6f7d84e647a4ecb0b49f4e31672248e19d1
jge <jge@codeaurora.org>
2016-09-05 15:05:30 +0800

    Wifi-HAL: Fix compile error for customer build
    Wcnss_service don't include stdlib.h, which causes
    implicit declaration of function 'atoi'.
    
    Issue happens when some customers download code without
    including /verdor/qcom/proprietary, so TARGET_USES_QCOM_WCNSS_QMI
    will not be defined, and stdlib.h can't be included indirectly.
    
    Change-Id: Ica7c55ff330bb32335f87556591f33b48076cf03
    CRs-Fixed: 1063287

--- wcnss-service/wcnss_service.c
+++ wcnss-service/wcnss_service.c
@@ -26,6 +26,7 @@ OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --------------------------------------------------------------------------*/
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
