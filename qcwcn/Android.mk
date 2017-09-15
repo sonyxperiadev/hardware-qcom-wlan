5d97dcd81e5d66bb916c616d3a460845d46d5813
Anne Ishani Fernando <anneis@codeaurora.org>
2017-09-15 11:21:06 -0700

    Removed condition that prevents this make file from running for Automotive target.
    Change-Id: Ie67783b37fbb5833f392eca48d6e50a340a1bb3a
    Signed-off-by: Anne Ishani Fernando <anneis@codeaurora.org>

--- qcwcn/Android.mk
+++ qcwcn/Android.mk
@@ -1,5 +1,3 @@
-ifneq ($(TARGET_BOARD_AUTO),true)
-  ifeq ($(BOARD_WLAN_DEVICE),qcwcn)
-    include $(call all-subdir-makefiles)
-  endif
+ifeq ($(BOARD_WLAN_DEVICE),qcwcn)
+  include $(call all-subdir-makefiles)
 endif
