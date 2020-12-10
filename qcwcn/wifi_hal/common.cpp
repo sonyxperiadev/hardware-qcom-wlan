ff6de98dd71490293d6fe15e54335a1fc88f49be
Sreeramya Soratkal <ssramya@codeaurora.org>
2020-12-10 11:53:27 +0530

    Resolve compilation error for fib_rule_port_range with fib_rules.h
    New members of type fib_rule_port_range are added in the structure
    rtnl_rule in netlink-private/types.h. This causes compilation error
    corresponding to the structure rtnl_rule of netlink-private/types.h
    which is included in wifi_hal. Update the header dependency in wifi_hal
    to include fib_rules.h to resolve compilation issues with
    fib_rules_port_range.
    
    CRs-Fixed: 2836075
    Change-Id: I83a7c55ee96f74a80c59caaa0e8a3437eaec60c0

--- qcwcn/wifi_hal/common.cpp
+++ qcwcn/wifi_hal/common.cpp
@@ -16,12 +16,12 @@
 
 #include <stdlib.h>
 #include <linux/pkt_sched.h>
+#include <linux-private/linux/fib_rules.h>
 #include <netlink/object-api.h>
 #include <netlink-private/object-api.h>
 #include <netlink-private/types.h>
 #include <dlfcn.h>
 #include <pthread.h>
-
 #include "wifi_hal.h"
 #include "common.h"
 #include <errno.h>
