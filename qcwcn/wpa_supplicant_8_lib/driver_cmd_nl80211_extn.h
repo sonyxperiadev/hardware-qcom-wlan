1a8765acd46d303887a8132c3bab53f628f53cb5
Mohammad Asaad Akram <asadkrm@codeaurora.org>
2021-01-17 11:11:10 +0530

    wpa_supplicant_8_lib: register wpa_msg callback for oem libraries
    This patch registers the wpa_msg_handler function to the oem shared
    libraries. With this oem libraries can print on wpa_cli console.
    
    Change-Id: Ie5869e4634dcf7f6e436e7f78249370506d5058f
    CRs-Fixed: 2854512

--- qcwcn/wpa_supplicant_8_lib/driver_cmd_nl80211_extn.h
+++ qcwcn/wpa_supplicant_8_lib/driver_cmd_nl80211_extn.h
@@ -51,6 +51,8 @@ typedef struct
 			char *cmd, char *buf, size_t buf_len, int *status);
     int (*wpa_driver_nl80211_driver_oem_event)(struct wpa_driver_nl80211_data *drv,
                                 u32 vendor_id, u32 subcmd, u8 *data, size_t len);
+    void (*wpa_driver_driver_wpa_msg_oem_cb)(void(*)(struct wpa_driver_nl80211_data *drv,
+								  char *msg));
 } wpa_driver_oem_cb_table_t;
 
 typedef wpa_driver_oem_cb_table_t* (wpa_driver_oem_get_cb_table_t)();
