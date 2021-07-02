eac1d47fd8127cb0f00033b2de7a1b82c2f3c5d5
Vinay Gannevaram <vganneva@codeaurora.org>
2021-07-03 00:02:07 +0530

    wifihal: Fetch lowi caps again if not obtained
    lowi server capabilities are fetched from fw
    after lowi initialization and wifi is turned on.
    When wifihal queries caps before wifi is on,
    it will receive no capabilities. Hence wifihal
    should retry for capabilites before sending
    lowi request
    
    CRs-Fixed: 2986700
    Change-Id: Ic8871794dbdd2d0a876449eb8116bad6095257bc

--- qcwcn/wifi_hal/common.cpp
+++ qcwcn/wifi_hal/common.cpp
@@ -386,6 +386,9 @@ lowi_cb_table_t *getLowiCallbackTable(u32 requested_lowi_capabilities)
                 "Returned error:%d. Exit.", __FUNCTION__, ret);
             goto cleanup;
         }
+    } else if (lowiSupportedCapabilities == 0 &&
+               LowiWifiHalApi->get_lowi_capabilities) {
+            LowiWifiHalApi->get_lowi_capabilities(&lowiSupportedCapabilities);
     }
 
     if ((lowiSupportedCapabilities & requested_lowi_capabilities) == 0) {
