c6c275e9f029a93223db3f4689701f963cd3881f
Eruvaram Kumar Raja Reddy <erajared@codeaurora.org>
2021-09-29 15:19:49 +0530

    WiFi-HAL: Remove unused libwpa_client shared library
    Remove libwpa-client shared library from WiFi-HAL
    as it is not used for any functionality.
    
    CRs-Fixed: 3047352
    Change-Id: I93671280b599c385569bf427aafe58d97c3337a1

--- qcwcn/wifi_hal/Android.mk
+++ qcwcn/wifi_hal/Android.mk
@@ -63,7 +63,6 @@ LOCAL_C_INCLUDES += \
 	external/libnl/include \
 	$(call include-path-for, libhardware_legacy)/hardware_legacy \
 	external/wpa_supplicant_8/src/drivers \
-	$(TARGET_OUT_HEADERS)/libwpa_client \
 	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	$(TARGET_OUT_HEADERS)/cld80211-lib
 
@@ -98,7 +97,7 @@ LOCAL_SRC_FILES := \
 LOCAL_MODULE := libwifi-hal-qcom
 LOCAL_VENDOR_MODULE := true
 LOCAL_CLANG := true
-LOCAL_SHARED_LIBRARIES += libnetutils liblog libwpa_client libcld80211
+LOCAL_SHARED_LIBRARIES += libnetutils liblog libcld80211
 
 ifneq ($(wildcard external/libnl),)
 LOCAL_SHARED_LIBRARIES += libnl
@@ -142,7 +141,6 @@ LOCAL_C_INCLUDES += \
 	external/libnl/include \
 	$(call include-path-for, libhardware_legacy)/hardware_legacy \
 	external/wpa_supplicant_8/src/drivers \
-	$(TARGET_OUT_HEADERS)/libwpa_client \
 	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	$(TARGET_OUT_HEADERS)/cld80211-lib
 
@@ -179,7 +177,7 @@ LOCAL_MODULE := libwifi-hal-qcom
 LOCAL_VENDOR_MODULE := true
 LOCAL_CLANG := true
 LOCAL_SHARED_LIBRARIES += libnetutils liblog
-LOCAL_SHARED_LIBRARIES += libdl libwpa_client libcld80211
+LOCAL_SHARED_LIBRARIES += libdl libcld80211
 LOCAL_SHARED_LIBRARIES += libwifi-hal-ctrl
 
 ifneq ($(wildcard external/libnl),)
