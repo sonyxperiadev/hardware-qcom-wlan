sonyxperiadev/hardware-qcom-wlan

commit 241180eed33be4a69c0b8a9c6b65f580dea11e23

wifi_hal: fix static analysis issues
Fix issues that opened socket or file descriptor
not closed in error handler.

Change-Id: Ibad9b2e7d2bffac41ba608f325b838894fedfffe
CRs-Fixed: 3204167

qcwcn/wifi_hal/wifi_hal.cpp
@@ -1263,6 +1263,7 @@ unload:
             }
             if (info->pkt_stats) free(info->pkt_stats);
             if (info->rx_aggr_pkts) free(info->rx_aggr_pkts);
+            if (info->wifihal_ctrl_sock.s) close(info->wifihal_ctrl_sock.s);
             wifi_logger_ring_buffers_deinit(info);
             cleanupGscanHandlers(info);
             cleanupRSSIMonitorHandler(info);
@@ -1300,12 +1301,14 @@ static int wifi_update_driver_state(const char *state) {
     if (fd < 0) {
         ALOGE("Failed to open driver state control param at %s",
               WIFI_DRIVER_STATE_CTRL_PARAM);
+        close(fd);
         return -1;
     }
     len = strlen(state) + 1;
     if (TEMP_FAILURE_RETRY(write(fd, state, len)) != len) {
         ALOGE("Failed to write driver state control param at %s",
               WIFI_DRIVER_STATE_CTRL_PARAM);
+        close(fd);
         ret = -1;
     }
     close(fd);

qcwcn/wifi_hal/wifi_hal_ctrl/wifi_hal_ctrl.c
@@ -114,9 +114,7 @@ try_again:
     pwd_system = getpwnam("system");
     uid_system = pwd_system ? pwd_system->pw_uid : 0;
     if (!gid_wifi || !uid_system) {
-        close(ctrl->s);
-        unlink(ctrl->local.sun_path);
-        free(ctrl);
+        wifihal_ctrl_close(ctrl);
         return NULL;
     }
     chown(ctrl->local.sun_path, -1, gid_wifi);
@@ -124,7 +122,7 @@ try_again:
 
 
     if (*ctrl_path != '/') {
-            free(ctrl);
+            wifihal_ctrl_close(ctrl);
             return NULL;
            }
 #endif /* ANDROID */
@@ -133,15 +131,12 @@ try_again:
        res = strlcpy(ctrl->dest.sun_path, ctrl_path,
                      sizeof(ctrl->dest.sun_path));
        if (res >= sizeof(ctrl->dest.sun_path)) {
-           close(ctrl->s);
-           free(ctrl);
+           wifihal_ctrl_close(ctrl);
            return NULL;
        }
        if (connect(ctrl->s, (struct sockaddr *) &ctrl->dest,
             sizeof(ctrl->dest)) < 0) {
-        close(ctrl->s);
-        unlink(ctrl->local.sun_path);
-        free(ctrl);
+        wifihal_ctrl_close(ctrl);
         return NULL;
        }
        /*

commit cdec4b1b192caddfeeb03d64bb71bc1367788aa0

wifi_hal: remove unnecessary file descriptor close calls
Remove unnecessary file descriptor close calls
in wifi_update_driver_state()

Fixes: 241180eed33be ("wifi_hal: fix static analysis issues")

Change-Id: Ia5b7ab19ab417b1afe9321fbb5970022a815ee0c
CRs-Fixed: 3283687

qcwcn/wifi_hal/wifi_hal.cpp
@@ -1301,14 +1301,12 @@ static int wifi_update_driver_state(const char *state) {
     if (fd < 0) {
         ALOGE("Failed to open driver state control param at %s",
               WIFI_DRIVER_STATE_CTRL_PARAM);
-        close(fd);
         return -1;
     }
     len = strlen(state) + 1;
     if (TEMP_FAILURE_RETRY(write(fd, state, len)) != len) {
         ALOGE("Failed to write driver state control param at %s",
               WIFI_DRIVER_STATE_CTRL_PARAM);
-        close(fd);
         ret = -1;
     }
     close(fd);