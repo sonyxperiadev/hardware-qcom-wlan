b948ac57c4178e8fd6e48f3357ba96fb069b8d22
Srikanth Marepalli <quic_srimarep@quicinc.com>
2022-10-26 18:57:03 +0530

    wifi_hal: Suppress the iface combination matrix errors in wearable
    These interface combination matrix changes are causing the
    compilation errors as the respective struct/enum definitions
    changes are not present on aosp branch.
    This change adds a wearable specific flag check to suppress the
    errors in the respective code.
    This change enables the framework in falling back to read the
    local static iface combination matrix table, instead of updating
    it from the driver.
    
    Change-Id: Ie1315bfacca6ac17f32a9030e59cd88aa8143c66
    CRs-Fixed: 3321402

--- qcwcn/wifi_hal/common.h
+++ qcwcn/wifi_hal/common.h
@@ -213,8 +213,10 @@ typedef struct hal_info_s {
     bool apf_enabled;
     bool support_nan_ext_cmd;
     pkt_log_version  pkt_log_ver;
+#ifndef TARGET_SUPPORTS_WEARABLES
     /* Interface combination matrix */
     wifi_iface_concurrency_matrix iface_comb_matrix;
+#endif /* TARGET_SUPPORTS_WEARABLES */
     qca_wlan_vendor_sar_version sar_version;
 } hal_info;
 
